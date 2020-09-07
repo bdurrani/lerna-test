f7bc385df0574bdff5b37d44daeb51e077310f3a
Bilal Durrani <bilal33@gmail.com>
2020-09-07 10:40:50 -0400

    save

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash 
+set -euo pipefail
+
+npx lerna version patch --yes --no-push
+readonly HEAD_SHA=$(git rev-parse --short HEAD)
+git tag "tag-${HEAD_SHA}"
+
+echo "Local change made. Wait for remote change"
+read year
+
+git pull origin --no-edit
+
+# git fetch origin
+# git merge develop -m "Merging changes"
+
+echo "Pushing to master"
+git push origin master --tags
+
+npx lerna publish from-package --yes
