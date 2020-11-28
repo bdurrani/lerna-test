5a029ac658c8505f3d66d51d298c5f69a5210d31
Bilal Durrani <bilal33@gmail.com>
2020-11-28 16:46:55 -0500

    make packages private

---
+++ packages/update-service/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const gitServicePackage = require("./package.json");
+const simpleGit = require("simple-git");
+
+const app = express();
+const port = 3001;
+const branch = "master";
+const git = simpleGit();
+
+app.get("/", async (_req, res) => {
+  try {
+    await git.pull("origin", branch, { "--no-rebase": null });
+    const status = await git.status();
+    res.send(status);
+  } catch (error) {
+    res.status(500);
+    res.send("error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(
+    `api v. ${gitServicePackage.version} listening at http://localhost:${port}`
+  );
+});
