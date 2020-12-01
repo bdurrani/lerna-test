151cc1576b6498678effae136b6ad2c192a16de2
Bilal Durrani <bilal33@gmail.com>
2020-11-30 21:52:29 -0500

    check before closing

--- packages/update-service/index.js
+++ packages/update-service/index.js
@@ -1,27 +1,48 @@
 const express = require("express");
 const gitServicePackage = require("./package.json");
 const simpleGit = require("simple-git");
+const { execSync } = require("child_process");
 require("dotenv").config();
+
+const { TOKEN, USERNAME, TARGET_DIR } = process.env;
 
 const app = express();
 const port = 3001;
 const branch = "master";
-const git = simpleGit();
-const { TOKEN, USERNAME, TARGET_DIR } = process.env;
+const git = simpleGit({
+  baseDir: TARGET_DIR,
+});
 const REPO = `https://${USERNAME}:${TOKEN}@github.com/bdurrani/lerna-test.git`;
 
+function isGitRepo(folder) {
+  try {
+    execSync(`git -C ${folder} rev-parse 2>/dev/null`);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
 (async () => {
-  const cloneResult = await git.clone(REPO, TARGET_DIR);
-  console.log(cloneResult);
+  if (!isGitRepo(TARGET_DIR)) {
+    console.log(`cloning to target dir ${TARGET_DIR}`);
+    await git.clone(REPO, TARGET_DIR);
+  } else {
+    console.log(`${TARGET_DIR} is already a git report`);
+  }
 
   app.get("/", async (_req, res) => {
     try {
-      await git.pull("origin", branch, { "--no-rebase": null });
+      console.log("updating repo");
+      await git.pull("origin", branch, {
+        "--no-rebase": null,
+      });
       const status = await git.status();
       res.send(status);
     } catch (error) {
       res.status(500);
-      res.send("error");
+      res.send(error);
+      console.log(`error: ${JSON.stringify(error)}`);
     }
   });
 
