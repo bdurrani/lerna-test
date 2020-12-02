a0cdd5c174744146738baf3c08546d1813b5b277
Bilal Durrani <bilal33@gmail.com>
2020-12-01 21:52:05 -0500

    test change

--- packages/runner/index.js
+++ packages/runner/index.js
@@ -4,7 +4,7 @@ const runnerPackage = require("./package.json");
 const packageB = require("@bdurrani/package-b");
 
 setInterval(() => {
-  console.log(`Running job. Version ${runnerPackage.version}`);
+  console.log(`Running job111. Version ${runnerPackage.version}`);
   packageB();
 }, intervalMs);
 
