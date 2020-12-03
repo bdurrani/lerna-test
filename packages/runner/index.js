58a348a32b97cf9fa42e6fcc5407c27f1d23e68a
Bilal Durrani <bilal33@gmail.com>
2020-12-02 21:41:06 -0500

    finite jobs

--- packages/runner/index.js
+++ packages/runner/index.js
@@ -2,10 +2,17 @@
 const intervalMs = 1000;
 const runnerPackage = require("./package.json");
 const packageB = require("@bdurrani/package-b");
+let count = 0;
+console.log("starting runner");
 
 setInterval(() => {
   console.log(`Running job22. Version ${runnerPackage.version}`);
   packageB();
+  count++;
+  if (count >= 4) {
+    console.log("job done");
+    process.exit(0);
+  }
 }, intervalMs);
 
 process.once("SIGUSR2", () => {
