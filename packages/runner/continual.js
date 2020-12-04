9503b17e2657f50fef5a67f8c72bba690a2f6605
Bilal Durrani <bilal33@gmail.com>
2020-12-03 22:19:54 -0500

    exit on finish

--- packages/runner/continual.js
+++ packages/runner/continual.js
@@ -1,16 +1,18 @@
 "use strict";
 
-// const forever = require("forever-monitor");
+const forever = require("forever-monitor");
 const nodemon = require("nodemon");
 
-const MAX_RUN_COUNT = 2;
+const MAX_RUN_COUNT = 5;
 
 nodemon({
   script: "index.js",
   ext: "js json",
   delay: 2000,
+  exitcrash: true,
 });
 let currentCount = 0;
+
 nodemon
   .on("start", function () {
     console.log("App has started");
@@ -18,23 +20,29 @@ nodemon
   .on("exit", function () {
     console.log("App has quit");
     currentCount++;
-    nodemon.restart();
-    if (currentCount >= MAX_RUN_COUNT) {
-      console.log("max restarts");
-      process.exit();
-    }
+    process.exit(0);
+    // setTimeout(() => {
+    //   nodemon.emit("restart");
+    // }, 1000);
+    // if (currentCount >= MAX_RUN_COUNT) {
+    //   console.log("max restarts");
+    //   process.exit(0);
+    // }
   })
-  .on("restart", function (files) {
+  .on("restart", (files) => {
     console.log("App restarted due to: ", files);
     // currentCount++;
   })
-  .on("crash", function (files) {
+  .on("crash", () => {
     console.log("App crashed");
     currentCount++;
+    process.exit(0);
   });
 
 // const child = new forever.Monitor("index.js", {
+// const child = new forever.Monitor("nodemon", {
 //   max: MAX_RUN_COUNT,
+//   command: "nodemon",
 // });
 
 // child.on("restart", () => {
